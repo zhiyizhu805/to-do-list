4b00a195d3d2931ef0ead1a42fdedcd85a573829
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-06 19:25:23 +1300

    TaskList component added

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,17 @@
   <h1>To DO LIST</h1>
   <input type="text" placeholder="New Task" />
   <button>Add</button>
-  <ul>
-    <event-list-item
-      v-for="task in AllTasks"
-      :key="task.taskId"
-      :taskId="task.taskId"
-      :taskName="task.taskName"
-      :isCompleted="task.isCompleted"
-    ></event-list-item>
-  </ul>
+  <task-list></task-list>
   <button>Clear Completed</button>
   <button>Clear All</button>
   <p>Pening Task:</p>
 </template>
 
 <script>
-import EventListItem from "./components/TaskListItem.vue";
+import TaskList from "./components/TaskList.vue";
 export default {
   components: {
-    EventListItem,
-  },
-  computed: {
-    AllTasks() {
-      return this.$store.getters.AllTasks;
-    },
+    TaskList,
   },
 };
 </script>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,3 +1,26 @@
 <template>
-    <div></div>
+  <ul>
+    <task-list-item
+      v-for="task in AllTasks"
+      :key="task.taskId"
+      :taskId="task.taskId"
+      :taskName="task.taskName"
+      :isCompleted="task.isCompleted"
+    ></task-list-item>
+  </ul>
 </template>
+
+<script>
+import TaskListItem from "./TaskListItem.vue";
+
+export default {
+  components: {
+    TaskListItem,
+  },
+  computed: {
+    AllTasks() {
+      return this.$store.getters.AllTasks;
+    },
+  },
+};
+</script>
