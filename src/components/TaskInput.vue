6adfb230c91d6cc2443c6fd478010b2e2b4420af
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-06 19:50:20 +1300

    TaskInput Component added

---
+++ src/components/TaskInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <form @submit.prevent="addTask">
+      <input type="text" placeholder="New Task" v-model="taskName" />
+      <input type="date" placeholder="New Task" v-model="taskDueDate" />
+      <button>Add</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      taskName: "",
+      taskDueDate: null,
+    };
+  },
+  methods: {
+    addTask() {
+      this.$store.dispatch("addTask", {
+        taskId: new Date().toISOString(),
+        taskName: this.taskName,
+        taskDueDate: this.taskDueDate,
+        isCompleted: false,
+      });
+    },
+  },
+};
+</script>
