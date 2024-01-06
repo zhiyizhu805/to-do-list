0e59af40fd8cd6cf7397e0d0bf141166a5781b10
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-06 21:45:59 +1300

    form validation added

--- src/components/TaskInput.vue
+++ src/components/TaskInput.vue
@@ -1,8 +1,10 @@
 <template>
   <section>
     <form @submit.prevent="addTask">
-      <input type="text" placeholder="New Task" v-model="taskName" />
-      <input type="date" placeholder="New Task" v-model="taskDueDate" />
+      <input type="text" placeholder="New Task" v-model.trim="taskName" @blur="clearValidity"/>
+      <p v-if="!isTaskNameValid">Task Name cannot be empty.</p>
+      <input type="date" placeholder="New Task" v-model.trim="taskDueDate" @blur="clearValidity"/>
+      <p v-if="!isTaskDueDateValid">Please select task due date.</p>
       <button>Add</button>
     </form>
   </section>
@@ -14,10 +16,33 @@ export default {
     return {
       taskName: "",
       taskDueDate: null,
+      isTaskNameValid: true,
+      isTaskDueDateValid: true,
+      isFormValid: true,
     };
   },
   methods: {
+    clearValidity() {
+      this.isTaskNameValid = true;
+      this.isTaskDueDateValid = true;
+    },
+    validateInput() {
+      if (this.taskName === "") {
+        this.isTaskNameValid = false;
+        this.isFormValid = false;
+      } if (this.taskDueDate === null) {
+        this.isTaskDueDateValid = false;
+        this.isFormValid = false;
+      }
+    },
     addTask() {
+      this.isTaskNameValid = true;
+      this.isTaskDueDateValid = true;
+      this.isFormValid = true;
+      this.validateInput();
+      if (!this.isFormValid) {
+        return;
+      }
       this.$store.dispatch("addTask", {
         taskId: new Date().toISOString(),
         taskName: this.taskName,
